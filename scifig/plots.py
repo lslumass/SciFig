704b9417a317998609ec88737862f8b72aaedb26
lslumass <shanlongli@umass.edu>
2024-05-24 10:41:11 -0400

    update

--- scifig/plots.py
+++ scifig/plots.py
@@ -10,7 +10,8 @@ def general_temp(num_row, num_col, size_x, size_y):
     mpl.rcParams['mathtext.default'] = 'regular'
     fig, axs = plt.subplots(num_row, num_col, figsize=(size_x, size_y))
     fig.tight_layout(pad=3)
-    axs = axs.ravel()
+    if num_row*num_col != 1:
+        axs = axs.ravel()
     for ax in axs:
         ax.tick_params(axis="both", direction='in', width=2, length=8.0, pad=6)
         plt.setp(ax.spines.values(), linewidth=2)
