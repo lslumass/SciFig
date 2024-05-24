4902cfcbcfa22364d0395aece58846ee3a7b5267
lslumass <shanlongli@umass.edu>
2024-05-24 16:54:03 -0400

    update

--- scifig/plots.py
+++ scifig/plots.py
@@ -7,16 +7,20 @@ def general_temp(num_row, num_col, size_x, size_y):
     create a matplotlib figure;
     plt.subplots(num_row, num_col, figsize=(size_x, size_y))
     '''
-    mpl.rcParams['mathtext.default'] = 'regular'
+
     fig, axs = plt.subplots(num_row, num_col, figsize=(size_x, size_y))
     fig.tight_layout(pad=3)
     if num_row*num_col != 1:
         axs = axs.ravel()
         for ax in axs:
-            ax.tick_params(axis="both", direction='in', width=2, length=8.0, pad=6)
+            ax.tick_params(axis="both", direction='in', width=2, length=8.0)
             plt.setp(ax.spines.values(), linewidth=2)
     else:
-        axs.tick_params(axis="both", direction='in', width=2, length=8.0, pad=6)
+        axs.tick_params(axis="both", direction='in', width=2, length=8.0)
         plt.setp(axs.spines.values(), linewidth=2)
+
+    plt.rcParams['font.family'] = "Arial"
     plt.rcParams['font.size'] = 18    
+    mpl.rcParams['mathtext.default'] = 'regular'
+
     return fig, axs
