7ba223cface9fe35f084041ab2d5d2f53a7202ed
lslumass <shanlongli@umass.edu>
2024-05-24 10:57:24 -0400

    update

--- scifig/plots.py
+++ scifig/plots.py
@@ -12,7 +12,10 @@ def general_temp(num_row, num_col, size_x, size_y):
     fig.tight_layout(pad=3)
     if num_row*num_col != 1:
         axs = axs.ravel()
-    for ax in axs:
-        ax.tick_params(axis="both", direction='in', width=2, length=8.0, pad=6)
-        plt.setp(ax.spines.values(), linewidth=2)
+        for ax in axs:
+            ax.tick_params(axis="both", direction='in', width=2, length=8.0, pad=6)
+            plt.setp(ax.spines.values(), linewidth=2)
+    else:
+        axs.tick_params(axis="both", direction='in', width=2, length=8.0, pad=6)
+        plt.setp(axs.spines.values(), linewidth=2)
     plt.rcParams['font.size'] = 18    
