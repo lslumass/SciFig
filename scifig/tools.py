f863db4ba9b2a68e3d4f0115fe84e364af062190
lslumass <shanlongli@umass.edu>
2024-06-27 09:21:07 -0400

    update

--- scifig/tools.py
+++ scifig/tools.py
@@ -7,8 +7,6 @@ def scatter2hist(point_list, num_bin, styles):
     styles:
         pdf: histogram of probability density function
         pmf: histogram of probability mass function
-        l_pdf: linked line of pdf
-        l_pmf: linked line of pmf
         s_pdf: smoothed line of pdf
         s_pmf: smoothed line of pmf
     '''
@@ -17,18 +15,12 @@ def scatter2hist(point_list, num_bin, styles):
     pdf = pmf/np.diff(bins)
 
     if styles == 'pdf':
-        bins = bins[:-1]+0.5*np.diff(bins)
-        return bins, pdf
-    elif styles == 'pmf':
-        bins = bins[:-1]+0.5*np.diff(bins)
-        return bins, pmf
-    elif styles == 'l_pdf':
         bins = np.insert(bins, 0, 2*bins[0]-bins[1])
         bins = bins+0.5*np.diff(bins)[0]
         pdf = np.insert(pdf, 0, 0)
         pdf = np.append(pdf, 0)
         return bins, pdf
-    elif styles == 'l_pmf':
+    elif styles == 'pmf':
         bins = np.insert(bins, 0, 2*bins[0]-bins[1])
         bins = bins+0.5*np.diff(bins)[0]
         pmf = np.insert(pmf, 0, 0)
